ecbc4dcef5d83d70805faa920f56953255a37194
khanh260802 <kedaumat460@gmail.com>
2023-12-26 00:41:49 +0700

    nothing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Route, Routes, BrowserRouter, Navigate} from "react-router-dom";
 import { useContext } from 'react';
 import { AuthContext } from './contexts/AuthProvider';
 function App() {
-
+  console.log(process.env.REACT_APP_apiKey)
   const currentUser = useContext(AuthContext);
   const ProtectedRoute = ({children}) => {
     if( !currentUser ) {
